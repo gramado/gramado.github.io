867f7a4beb4c12a4b2c2458852d7a2582d493a21
frednora <freddynora@gmail.com>
2023-07-18 10:57:20 -0300

    gramado.js: Create GramadoClass.

--- assets/js/gramado.js
+++ assets/js/gramado.js
@@ -1,9 +1,29 @@
 
+class GramadoClass {
+    constructor (s){
+        console.log("Gramado: ");
+        console.log(s);
+    }
+    printString(s){
+        console.log(s);
+    }
+};
+let Gramado;
 let __gramado_status = 0;
 
 function gramadoInitialize(){
     let ReturnValue=1234;
     console.log("Initializing Gramado ...");
+
+// Creating the Gramado class.
+    Gramado = new GramadoClass("Initializing GramadoClass...");
+    Gramado.printString("Testing Gramado.printString method ...");
+
+    //arrowFunction1(1);
+    //arrowFunction2(2);
+    //arrowFunction3(3);
+    //arrowFunction4();
+
     __gramado_status = 1;
     return ReturnValue;
 }
@@ -16,6 +36,25 @@ function gramadoHelloWorld(){
         window.alert("Not initialized");
     }
 }
+
+/* Learning ... */
+let arrowFunction1 = (x) => {
+    console.log("arrowFunction1: ");
+    console.log(x);
+}
+
+/* Learning ... */
+let arrowFunction2 = (x) => console.log(x)
+
+/* Learning ... */
+let arrowFunction3 = x => console.log(x)
+
+/* Learning ... */
+let arrowFunction4 = () => console.log("arrowFunction4: ")
+
+
+
+
 
 
 
