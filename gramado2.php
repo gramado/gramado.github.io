8f0d1552ced0ee4d64315dbd30c2e12b5429d152
Fred Nora <freddynora@gmail.com>
2024-07-09 11:38:12 -0300

    Learning PHP.

---
+++ gramado2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Gramado</title>
+<link href="./assets/css/gramado.css" rel="stylesheet">
+</head>
+
+<body onload="gramadoInitialize()">
+
+<?php 
+    echo "<h1>Hello world!</h1>";
+?>
+
+<script src="./assets/js/gramado.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php 
 define('USE_XXX', true);
+/*require('./gramado2.php');*/
 require('./gramado.php');
 ?>
