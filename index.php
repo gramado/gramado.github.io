fe7b42d25b6bed43eb1fa5f3c5555534c1739879
Fred Nora <freddynora@gmail.com>
2024-07-13 09:36:03 -0300

    Webpage: Update main interface.

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
 define('USE_XXX', true);
 /*require('./gramado2.php');*/
-require('./gramado.php');
+/*require('./gramado.php');*/
+require('./search/search.php');
 ?>
---
+++ search/search.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<!-- Document created by Fred Nora -->
+<!-- Credits: A. de Regt -->
+
+<html lang="en">
+
+<head>
+<meta charset="UTF-8">
+<title>Gramado</title>
+
+    <meta 
+        name="viewport" 
+        content="width=device-width, initial-scale=1.0">
+    <meta 
+        name="description" 
+        content="Gramado Organization">
+    <meta 
+        name="author" 
+        content="Fred Nora">
+
+<style>
+            /*
+            body {
+                background-color: green
+            }
+            */
+
+            .core {
+                position: absolute;
+                left: 50%;
+                top: 50%;
+                transform: translate(-50%, -50%);
+                border-radius: .33rem;
+                padding: 10px;
+                background-color: lightyellow; /*#44ab44;*/
+                box-shadow: 10px 10px lightgray;
+                text-align: center;
+            }
+
+            .button {
+                border: solid black 1px;
+                border-radius: .25rem;
+                padding: 10px;
+                color: black;
+                text-decoration: auto;
+                text-align: center;
+            }
+
+            .noicon {
+                padding-left: 30px;
+            }
+
+            .iconpos {
+            }
+
+            .button:hover{
+                background-color: lightyellow;
+            }
+</style>
+
+<!-- Prefetch -->
+<link rel="dns-prefetch" href="https://github.com/"/>
+
+<!-- Cool links -->
+<link href="./assets/css/gramado.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <!-- Navigation bar -->
+    <nav class="gramado-nav">
+        <ul>
+            <!-- 1: Developer -->
+            <li><a href="#section-1">Developer</a></li>
+            <!-- 2: Business person -->
+            <li><a href="#section-2">Business</a></li>
+            <!-- 3: Sales person -->
+            <li><a href="#section-3">Online communication</a></li>
+            <!-- 4: User -->
+            <li><a href="#section-4">User</a></li>
+        </ul>
+    </nav>
+
+    <!-- Core module -->
+    <div class="core">
+    <h1>Gramado Organization</h1>
+
+    <br>
+    <?php
+        $files = scandir(getcwd());
+        foreach ($files as $file)
+        {
+            if ( is_dir($file) && ctype_alnum($file) )
+            {
+    ?>
+
+                <!-- Button -->
+                <br/>
+                <a href="/<?php echo $file; ?>/index.html"><?php echo $file; ?>/</a>
+
+    <?php
+            }
+        }
+    ?>
+
+        <!-- Button -->
+        <br/>
+        <br/>
+        <a 
+            class="button" 
+            href="https://github.com/gramado/gramado.github.io">
+            <img class="iconpos" 
+                width="20" 
+                height="20" 
+                src="https://github.com/fluidicon.png"> See code at GitHub
+        </a>
+        
+        <br/>
+        <br/>
+        <br/>
+    </div>
+
+<!-- Cool scripts -->
+<script src="./assets/js/gramado.js"></script>
+</body>
+
+</html>
